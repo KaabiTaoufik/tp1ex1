6a589ee0efa48e6e86bae6b8e51c5ae33eed13e9
Kaabi Taoufik <taoufik_c10@hotmail.com>
2021-03-13 08:00:27 +0000

    final

--- Tp1/script.js
+++ Tp1/script.js
@@ -1,12 +1,12 @@
 "use strict";
 let a = Math.round(Math.random() * 10);
-const b = prompt("Choisir la difficulté");
+const b = prompt("Choisir la difficulté (nombre d'essai)");
 for (let i = 0; i < b; i++) {
   console.log(a);
   let d = prompt("Devinez");
   console.log(d);
   if (a == d) {
-    alert("Vous avez gangé");
+    alert("Vous avez gangé à l'essai' "+i +" votre score est : "+b-i);
     break;
   }
 }
